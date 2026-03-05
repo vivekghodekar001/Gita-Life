ee028981602305355e9e8d39cfd494f89ae57ac5
vivekghodekar001 <vivekghodekar001@gmail.com>
2026-03-05 08:34:52 +0530

    Update .gitignore and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
     content: [
         "./index.html",
-        "./**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./App.tsx",
+        "./index.tsx",
     ],
     theme: {
         extend: {
